8e78981c85b1898393d4f01a5d60baffa91e384f
Agboola Tobi Samuel <tobi4tee@gmail.com>
2024-05-21 14:45:57 +0100

    Commit made

--- src/main/java/com/serviceFusion/Capstone/controllers/AdminController.java
+++ src/main/java/com/serviceFusion/Capstone/controllers/AdminController.java
@@ -1,14 +1,11 @@
 package com.serviceFusion.Capstone.controllers;
 
 
-import com.serviceFusion.Capstone.data.models.Admin;
 import com.serviceFusion.Capstone.dtos.requests.AdminRegistrationRequest;
 import com.serviceFusion.Capstone.dtos.responses.AdminRegistrationResponse;
 import com.serviceFusion.Capstone.exceptions.ServiceFusionException;
 import com.serviceFusion.Capstone.services.AdminService;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,15 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/admin")
+@RequestMapping("api/v1/admin/")
 @AllArgsConstructor
 public class AdminController {
     private AdminService adminService;
-    private final Logger logger = LoggerFactory.getLogger(AdminController.class);
+//    private final Logger logger = LoggerFactory.getLogger(AdminController.class);
 
-    @PostMapping
+    @PostMapping("register")
     public ResponseEntity<AdminRegistrationResponse> addAdmin(@RequestBody AdminRegistrationRequest request) throws ServiceFusionException {
-        logger.trace("Entering addAdmin method");
+//        logger.trace("Entering addAdmin method");
         return ResponseEntity.status((HttpStatus.CREATED))
                 .body(adminService.registerAdmin(request));
     }
